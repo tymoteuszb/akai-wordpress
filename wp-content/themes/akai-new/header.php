4a834934a146b243d4cc271470b4e72ca735e2f9
Jacek Tomaszewski <kontakt@jtomaszewski.eu>
2014-04-20 14:47:21 +0200

    Add wpsso plugin

--- wp-content/themes/akai-new/header.php
+++ wp-content/themes/akai-new/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php wp_title( '|', true, 'right' ); ?></title>
+  <title><?php wp_title( '|', true, 'right' ); ?><?php echo get_bloginfo('name'); ?></title>
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <link rel="alternate" type="application/rss+xml" title="AKAI - Wydarzenia" href="<?= esc_attr_e(RSS_LINK); ?>" />
